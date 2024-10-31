9b2034633b07cb54191c5834ecef36c390985cfc
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2024-10-31 08:29:33 -0400

    Update load_balancer values

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,6 +1,6 @@
 module "load_balancer" {
   count = try(var.vmss.lb, null) != null ? 1 : 0
-  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-load_balancer.git"
+  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-load_balancer.git?ref=v1.0.2"
 
   location          = var.location
   subnets           = var.subnets
@@ -10,7 +10,7 @@ module "load_balancer" {
   env               = var.env
   group             = var.group
   project           = var.project
-  load_balancer      = var.vmss.lb
+  load_balancer     = merge(var.vmss.lb, { postfix = var.vmss.postfix, resource_group_name = var.vmss.resource_group_name, custom_name = local.vmss_name })
   custom_data       = try(var.vmss.lb.custom_data, false) != false ? base64encode(file("${path.cwd}/${var.vmss.lb.custom_data}")) : null
   user_data         = try(var.vmss.lb.user_data, false) != false ? base64encode(file("${path.cwd}/${var.vmss.lb.user_data}")) : null
 }
